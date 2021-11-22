8ac29ff0e86d9f50d850fdc12ed8be3142602314
boraozer <nagayapim@gmail.com>
2021-11-22 03:54:27 +0300

    change service instance to global variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const server = require('./src/server')
-const service = new server()
+service = new server() // global instance
 
 service.open()
 service.initSocketConnection()

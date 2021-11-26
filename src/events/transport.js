a82f51a1726457ffd3b8a60d974826285fdf6aab
boraozer <nagayapim@gmail.com>
2021-11-27 02:52:26 +0300

    bridge transport event listener base

--- src/events/transport.js
+++ src/events/transport.js
@@ -1,9 +1,13 @@
-async function listener(socket, data)
+async function listener(socket, eventPackage)
 {
-    
+    const {id} = socket
+    service.io.to('signall-area').emit('transport', {
+        package: eventPackage,
+        buyer: id
+    });
 }
 
 module.exports = {
     name: 'transport',
-    listener: listener
+    listener: listener  
 }

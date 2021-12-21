d2031e6860ec77139a69db019d14a5387bebb0d6
boraozer <nagayapim@gmail.com>
2021-12-22 02:16:37 +0300

    transport event added eventSimulation pooling

--- src/events/transport.js
+++ src/events/transport.js
@@ -1,10 +1,24 @@
-async function listener(socket, eventPackage)
+const { v4: uuidv4 } = require('uuid')
+
+async function listener(socket, {livingTime, ...eventPackage})
 {
     const {id} = socket
+    const poolingListenerName = uuidv4()
     service.io.to('signall-area').emit('transport', {
         package: eventPackage,
-        buyer: id
-    });
+        bridgeBuyer: id,
+        poolingListenerName: poolingListenerName
+    })
+
+    socket.on(poolingListenerName, (data)=> {
+        console.log(data)
+    })
+
+    setTimeout(()=> {
+        socket.off(poolingListenerName)
+    }, livingTime)
+
+
 }
 
 module.exports = {

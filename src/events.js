2dbe26a46521d34ffde50497b616e3da55878a7d
boraozer <nagayapim@gmail.com>
2021-12-31 05:31:59 +0300

    add transport-pooling socket listener

--- src/events.js
+++ src/events.js
@@ -2,5 +2,6 @@ module.exports = [
     require('./events/register'),
     require('./events/transport'),
     require('./events/disconnect'),
-    require('./events/findSignallingServer')
+    require('./events/findSignallingServer'),
+    require('./events/transportPooling')
 ]
